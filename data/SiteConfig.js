b3e88c50a74215cc66276f31f20e7cb6d8a6be57
Tyler Hall <0tillathehun0@gmail.com>
2019-10-20 01:32:30 -0400

    chore: update sitemap

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -4,8 +4,8 @@ const config = {
   siteTitleShort: 'Tilla Blog', // Short site title for homescreen (PWA). Preferably should be under 12 characters to prevent truncation.
   siteTitleAlt: 'TillaTheHun0 Blog', // Alternative site title for SEO.
   siteLogo: '/logos/logo-1024.png', // Logo used for SEO and manifest.
-  siteUrl: 'https://vagr9k.github.io', // Domain of your website without pathPrefix.
-  pathPrefix: '/', // Prefixes all links. For cases when deployed to example.github.io/gatsby-advanced-starter/.
+  siteUrl: 'https://tillathehun0.github.io/TillaTheBlog0', // Domain of your website without pathPrefix.
+  pathPrefix: '', // Prefixes all links. For cases when deployed to example.github.io/gatsby-advanced-starter/.
   siteDescription: 'A Blog For me to talk about things I like. Built with Gatsby', // Website description used for RSS feeds/meta description tag.
   siteRss: '/rss.xml', // Path to the RSS file.
   siteFBAppID: '', // FB Application ID for using app insights
